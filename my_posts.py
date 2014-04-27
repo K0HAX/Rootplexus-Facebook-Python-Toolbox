52532f3e3aa1d1835b0d6b6b09e4079f5eed0409
Michael Englehorn <Michael@englehorn.com>
2014-04-27 01:56:32 -0500

    Added my_posts.py Signed-off-by: Michael Englehorn <Michael@englehorn.com>

--- my_posts.py
+++ my_posts.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 import facebook
-import sys
 
 def file_get_contents(filename):
         with open(filename) as f:
@@ -10,23 +9,17 @@ token = file_get_contents(".fb_access_token")
 
 graph = facebook.GraphAPI(token)
 profile = graph.get_object("me")
-news_feed = graph.get_connections("me", "home")
+news_feed = graph.get_connections("me", "posts")
 
 for post in reversed(news_feed['data']):
-	try:
-		print post['from'].get("name")
-		print post.get("message")
-	except:
-		e = sys.exc_info()[0]
+	print post['from'].get("name")
+	print post.get("message")
 	print "Created: " + post.get("created_time")
 	print "Post ID: " + post.get("id")
 	feed_comment = graph.get_connections(post.get("id"), "comments")
 	for comment in feed_comment['data']:
-		try:
-			print "++++"
-			print comment['from'].get("name")
-			print comment.get("message")
-			print "Created: " + comment.get("created_time")
-		except:
-			e = sys.exc_info()[0]
+		print "++++"
+		print comment['from'].get("name")
+		print comment.get("message")
+		print "Created: " + comment.get("created_time")
 	print "------------------"
